d1cc48ccd83832943f5bb24bd67d25b0af76c39a
Igor <igor.novodon@gmail.com>
2026-04-06 16:18:09 +0300

    console output

---
+++ src/out/console-logger/console-logger.ts
@@ -0,0 +1,24 @@
+import { IStreamLogger } from "../../core/handlers/steam-logger.interface";
+
+class ConsoleLogger implements IStreamLogger {
+
+    private static logger: ConsoleLogger
+    public static getInstance() {
+        if (!ConsoleLogger.logger) {
+            ConsoleLogger.logger = new ConsoleLogger()
+        }
+        return ConsoleLogger.logger
+    }
+    error(...args: any[]): void {
+        console.log(args)
+    }
+
+    log(...args: any[]): void {
+        console.log(args)
+    }
+
+    end(): void {
+        console.log('готово')
+    }
+
+}
